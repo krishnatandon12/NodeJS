b58f67d8a913061342aae3e879c993ff29350a2b
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-16 02:41:09 +0530

    Node JS - Hello World

---
+++ 1_Intro.js
@@ -0,0 +1,12 @@
+//First NodeJS program. 
+//Check whether a number is greater or smaller than the current number.
+
+const num = 12;
+
+if (num < 10) {
+    console.log("Number is smaller");
+} else {
+    console.log("Number is greater");
+}
+
+console.log(`Hello NodeJS, this is my first Node Program.`);
