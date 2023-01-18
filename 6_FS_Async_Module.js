81a44fb7e961d7b3b7cb5ffac97e719c5971a702
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-18 20:36:57 +0530

    FS_Async Module

---
+++ 6_FS_Async_Module.js
@@ -0,0 +1,26 @@
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf8", (result, err) => {
+    if (err) {
+        console.log(err);
+        return "";
+    }
+    const first = result;
+
+    readFile("./content/second.txt", "utf8", (err, result) => {
+        if (err) {
+            console.log(err);
+            return err;
+        }
+        const second = result;
+
+        writeFile("./content/result-async.txt", `Merged - ${first} and ${second}`, (err, result) => {
+            if (err) {
+                console.log(err);
+                return err;
+            }
+            console.log(result); //writeFile callback.
+        });
+    }); //second.txt callback ends.
+
+}); //first.txt callback ends.
